952addf799d255677622bc8ade5851fcdc142506
liuuu <next@yudeMacBook-Air.local>
2018-01-16 21:01:54 +0800

    add User schema&resolver for signup

--- server/src/email-password/signup.ts
+++ server/src/email-password/signup.ts
@@ -10,6 +10,7 @@ interface User {
 interface EventData {
   email: string;
   password: string;
+  name: string;
 }
 
 const SALT_ROUNDS = 10;
@@ -21,7 +22,7 @@ export default async (event: FunctionEvent<EventData>) => {
     const graphcool = fromEvent(event);
     const api = graphcool.api('simple/v1');
 
-    const { email, password } = event.data;
+    const { email, password, name } = event.data;
 
     if (!validator.isEmail(email)) {
       return { error: 'Not a valid email' };
@@ -38,7 +39,7 @@ export default async (event: FunctionEvent<EventData>) => {
     const hash = await bcrypt.hash(password, salt);
 
     // create new user
-    const userId = await createGraphcoolUser(api, email, hash);
+    const userId = await createGraphcoolUser(api, email, hash, name);
 
     // generate node token for new User node
     const token = await graphcool.generateNodeToken(userId, 'User');
@@ -69,13 +70,15 @@ async function getUser(api: GraphQLClient, email: string): Promise<{ User }> {
 async function createGraphcoolUser(
   api: GraphQLClient,
   email: string,
-  password: string
+  password: string,
+  name: string
 ): Promise<string> {
   const mutation = `
-    mutation createGraphcoolUser($email: String!, $password: String!) {
+    mutation createGraphcoolUser($email: String!, $password: String!, $name: String!) {
       createUser(
         email: $email,
         password: $password
+        name: $name
       ) {
         id
       }
@@ -85,6 +88,7 @@ async function createGraphcoolUser(
   const variables = {
     email,
     password: password,
+    name,
   };
 
   return api.request<{ createUser: User }>(mutation, variables).then(r => r.createUser.id);
